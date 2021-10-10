5453c11935270b55ed6018407b373514311bf2df
Ammon Johnson <77296555+Ammon-J@users.noreply.github.com>
2021-10-09 22:54:18 -0700

    Changed DB connection string and Created Character table

---
+++ Data/Migrations/20211010055127_AddedCharactersTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASP.NET_DnD_App.Data.Migrations
+{
+    public partial class AddedCharactersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "characters",
+                columns: table => new
+                {
+                    CharacterId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CharacterName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClassName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescriptionId = table.Column<int>(type: "int", nullable: false),
+                    CharacterSheetId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_characters", x => x.CharacterId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "characters");
+        }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_DnD_App.Models
+{
+    public class Character
+    {
+        [Key]
+        [Required]
+        public int CharacterId { get; set; }
+
+        [Required]
+        public string? CharacterName { get; set; }
+
+        [Required]
+        public string? ClassName {  get; set;}
+
+        public int DescriptionId { get; set; }
+
+        public int CharacterSheetId { get; set; }
+    }
+}
