688b06207386dc5e3230941e568dc820eafce680
cloverspark <alexbrenna1999@hotmail.com>
2021-11-20 11:12:18 -0800

    Added Race Controller

---
+++ Controllers/RacesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_DnD_App.Controllers
+{
+    public class RacesController : Controller
+    {
+        public IActionResult Elves()
+        {
+            return View();
+        }
+    }
+}
