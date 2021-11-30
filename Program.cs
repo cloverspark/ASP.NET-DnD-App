2dd9633caec02ac63bac4294094fb5efa15888da
Ammon Johnson <77296555+Ammon-J@users.noreply.github.com>
2021-11-29 16:43:37 -0800

    Implemented Sendgrid Correctly

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ASP.NET_DnD_App.Data;
+using ASP.NET_DnD_App.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddTransient<IEmailProvider, SendGridEmailProvider>();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
