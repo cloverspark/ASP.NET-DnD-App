ab89f5f9bf96af476fd158555130af6ee3d9bb4e
Ammon Johnson <77296555+Ammon-J@users.noreply.github.com>
2022-01-19 16:06:07 -0800

    Set up user roles

---
+++ IdentityHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PC2.Data
+{
+    public static class IdentityHelper
+    {
+        public const string Admin = "Admin";
+
+        internal static async Task CreateRoles(IServiceProvider provider, params string[] roles)
+        {
+            var roleManager = provider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            IdentityResult roleResult;
+
+            foreach(string role in roles) 
+            {
+                bool doesRoleExist = await roleManager.RoleExistsAsync(role);
+                if (!doesRoleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
