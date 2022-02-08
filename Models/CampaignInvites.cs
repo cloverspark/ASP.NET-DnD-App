5dd897fb8fb408c07b1765c3dc3dc95fc28bc447
Ammon Johnson <77296555+Ammon-J@users.noreply.github.com>
2022-02-08 11:09:00 -0800

    Creates CampaignInvites, Campaigns, and CampignPlayers tables to DB

---
+++ Models/CampaignInvites.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_DnD_App.Models
+{
+    // When a DuneonMaster sends an invite to a campaign to a BasicPlayer the invite information gets put into this 
+    // table until the BasicPlayer accepts this invite. (Invite info will be deleted from this table when
+    // invite was accepted)
+    public class CampaignInvites
+    {
+        [Key]
+        [Required]
+        public int InviteId { get; set; }
+
+        [Required]
+        public IdentityUser DungeonMaster { get; set; } // Dungeon master of the campaign
+
+        [Required]
+        public int InviteCode { get; set; } // Use this code to join a game. It is only used to let invited people to the campaign
+
+        [Required]
+        public string? InvitedPlayerUserName { get; set; } // This is used to check if the user who gets the code was actually invited 
+    }
+}
---
+++ Models/CampaignPlayers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_DnD_App.Models
+{
+    public class CampaignPlayers
+    {
+        [Key]
+        [Required]
+        public int PlayerId { get; set; }
+
+        [Required]
+        public Campaigns CampaignsId { get; set; }
+
+        public IdentityUser BasicPlayer { get; set; }
+    }
+}
