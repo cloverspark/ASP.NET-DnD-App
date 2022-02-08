6dae168e60d612c1e3cfb942e2baa598fd815560
Ammon Johnson <77296555+Ammon-J@users.noreply.github.com>
2022-02-08 14:33:08 -0800

    Added Campaign controller

---
+++ Controllers/CampaignController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_DnD_App.Controllers
+{
+    public class CampaignController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
